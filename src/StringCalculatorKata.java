91216924047121921c1c34a93a634c1dc318b5f8
Murielle <muriellehyperskill@gmail.com>
2021-09-14 12:25:50 +0200

    add with one comma

--- src/StringCalculatorKata.java
+++ src/StringCalculatorKata.java
@@ -1,4 +1,26 @@
+import java.util.Arrays;
+
+
 public class StringCalculatorKata {
 
-    
+   public int add(String numbers) {
+       if(numbers.isEmpty()){
+           return 0;
+       }else if(numbers.length() == 1){
+           return Integer.parseInt(numbers);
+       }else {
+           return Arrays.stream(numbers.split(","))
+                   .mapToInt(Integer::parseInt)
+                   .sum();
+       }
+
+   }
+
+    public int add(String... numbers) {
+        return 0;
+    }
+
+    /*public int Add(String numbers) {
+        return 0;
+    }*/
 }
--- src/StringCalculatorKataTest.java
+++ src/StringCalculatorKataTest.java
@@ -1,2 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringCalculatorKataTest {
+    private StringCalculatorKata stringCalculatorKata = new StringCalculatorKata();
+
+    @Test
+    public void shouldReturn0ForNumbersIsEmpty(){
+          Assert.assertEquals(stringCalculatorKata.add(""), 0);
+    }
+
+    @Test
+    public void shouldReturn3ForNumbersIs3(){
+        Assert.assertEquals(stringCalculatorKata.add("3"), 3);
+    }
+
+    @Test
+    public void shouldReturn6ForNumbersIsNotEmpty(){
+        Assert.assertEquals(stringCalculatorKata.add("1,2,3"), 6);
+    }
 }
