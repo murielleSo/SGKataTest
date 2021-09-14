26e6d00d692296233bb5dab004b0e304e8445f3a
Murielle <muriellehyperskill@gmail.com>
2021-09-15 01:32:43 +0200

    reformat code

--- src/StringCalculatorKata.java
+++ src/StringCalculatorKata.java
@@ -1,21 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 public class StringCalculatorKata {
 
-   public int add(String numbers) {
-           if(numbers.isEmpty()){
-               return 0;
-           }else if(numbers.length() == 1){
-               return Integer.parseInt(numbers);
-           }else {
-               return sum(splitNumbers(numbers, ","));
-           }
-   }
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        } else if (numbers.length() == 1) {
+            return Integer.parseInt(numbers);
+        } else {
+            return sum(splitNumbers(numbers, ","));
+        }
+    }
 
     public int add(String... numbers) {
         List<String> list = new ArrayList<>();
@@ -33,36 +32,36 @@ public class StringCalculatorKata {
     }
 
     public int addWithAnyDelimeter(String numbers) {
-       if(numbers.startsWith("//")) {
-           String delimeter = numbers.substring(2,numbers.indexOf("\n"))+"|\n";
+        if (numbers.startsWith("//")) {
+            String delimeter = numbers.substring(2, numbers.indexOf("\n")) + "|\n";
 
-           String newNumbers = numbers.substring(numbers.indexOf("\n") - 1,numbers.length());
+            String newNumbers = numbers.substring(numbers.indexOf("\n") - 1, numbers.length());
 
-           return sum(splitNumbers(newNumbers, delimeter)
-                      .filter(numb -> !"".equals(numb)));
-       }
-       return 0;
+            return sum(splitNumbers(newNumbers, delimeter)
+                    .filter(numb -> !"".equals(numb)));
+        }
+        return 0;
     }
 
     public void addthrowsExeptionWithNegativeNumbers(String numbers) {
-       List<Integer> list = new ArrayList<>();
-         splitNumbers(numbers, ",")
-                 .mapToInt(Integer::parseInt)
-                 .filter(numb -> numb < 0)
-                 .forEach(negative -> list.add(negative));
+        List<Integer> list = new ArrayList<>();
+        splitNumbers(numbers, ",")
+                .mapToInt(Integer::parseInt)
+                .filter(numb -> numb < 0)
+                .forEach(negative -> list.add(negative));
 
-         if (!list.isEmpty()) {
-             throw new IllegalArgumentException("negatives not allowed " + list.toString());
-         }
+        if (!list.isEmpty()) {
+            throw new IllegalArgumentException("negatives not allowed " + list.toString());
+        }
     }
 
     private static Stream<String> splitNumbers(String numbersToSplit, String delimeter) {
-       return Arrays.stream(numbersToSplit.split(delimeter));
+        return Arrays.stream(numbersToSplit.split(delimeter));
     }
 
     private static int sum(Stream<String> streamNumbers) {
-       return streamNumbers
-               .mapToInt(Integer::parseInt)
-               .sum();
+        return streamNumbers
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
--- src/StringCalculatorKataTest.java
+++ src/StringCalculatorKataTest.java
@@ -5,45 +5,45 @@ public class StringCalculatorKataTest {
     private final StringCalculatorKata stringCalculatorKata = new StringCalculatorKata();
 
     @Test
-    public void shouldReturn0ForNumbersIsEmpty(){
-          Assert.assertEquals(stringCalculatorKata.add(""), 0);
+    public void shouldReturn0ForNumbersIsEmpty() {
+        Assert.assertEquals(stringCalculatorKata.add(""), 0);
     }
 
     @Test
-    public void shouldReturn3ForNumbersIs3(){
+    public void shouldReturn3ForNumbersIs3() {
         Assert.assertEquals(stringCalculatorKata.add("3"), 3);
     }
 
     @Test
-    public void shouldReturn3ForNumbersIsNotEmpty(){
+    public void shouldReturn3ForNumbersIsNotEmpty() {
         Assert.assertEquals(stringCalculatorKata.add("1,2"), 3);
     }
 
     @Test
-    public void shouldReturn26ForUnknowNumbers(){
+    public void shouldReturn26ForUnknowNumbers() {
         Assert.assertEquals(stringCalculatorKata.add("1,2,3", "10,10"), 26);
     }
 
     @Test
-    public void shouldReturn6ForTwoDelimeter(){
+    public void shouldReturn6ForTwoDelimeter() {
         Assert.assertEquals(stringCalculatorKata.addWithTwoCommaDelimeter("1\n2,3"), 6);
     }
 
     @Test
-    public void shouldReturn3ForAnyDelimeter(){
+    public void shouldReturn3ForAnyDelimeter() {
         Assert.assertEquals(stringCalculatorKata.addWithAnyDelimeter("//;\n1;2"), 3);
     }
 
     @Test
-    public void shouldReturn6ForAnyDelimeter(){
+    public void shouldReturn6ForAnyDelimeter() {
         Assert.assertEquals(stringCalculatorKata.addWithAnyDelimeter("//&\n1&2&3"), 6);
     }
 
     @Test
-    public void shouldThrowsExceptionWithNegativesValues(){
+    public void shouldThrowsExceptionWithNegativesValues() {
         try {
             stringCalculatorKata.addthrowsExeptionWithNegativeNumbers("1,-2,-5,4");
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             Assert.assertEquals(e.getMessage(), "negatives not allowed [-2, -5]");
         }
 
